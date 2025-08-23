02df366dc8f349293079d9d128ecb1cf3c9b8926
Sekh Sujon Haque <sksujonhaque@gmail.com>
2025-08-23 20:35:15 +0000

    you are not adding api key

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,6 +3,7 @@ import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [googleAI({
+    apiKey: process.env.GEMINI_API_KEY,
     // Explicitly disable gemini-pro from being the default model.
     // We want to select the model in the prompt definition.
     defaultModel: '',
