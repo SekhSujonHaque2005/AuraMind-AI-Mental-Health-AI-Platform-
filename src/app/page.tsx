3109703c1a819473fe6244fee0197e2bf38ba4ba
Sekh Sujon Haque <sksujonhaque@gmail.com>
2025-09-04 14:26:35 +0000

    add ballfit background only in chat Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 
+import Ballpit from '@/components/ballpit';
 import ChatInterface from '@/components/chat-interface';
 
 export default function Home() {
   return (
     <div className="relative flex flex-col h-screen w-full">
-        <ChatInterface />
+        <div className="absolute inset-0 z-0">
+            <Ballpit />
+        </div>
+        <div className="relative z-10 flex flex-col h-full">
+            <ChatInterface />
+        </div>
     </div>
   );
 }
