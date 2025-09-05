bdcf2d91b4202dca89ab271e82f6a76832223ad4
Sekh Sujon Haque <sksujonhaque@gmail.com>
2025-09-05 20:29:02 +0000

    i Want Same Designing in chat Page also along with ballpit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,11 @@ import ChatInterface from '@/components/chat-interface';
 
 export default function Home() {
   return (
-    <div className="relative flex flex-col h-screen w-full">
+    <div className="relative flex flex-col h-screen w-full overflow-hidden">
+        <div className="absolute inset-0 -z-10 h-full w-full">
+            <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]"></div>
+            <div className="absolute left-0 right-0 top-[-10%] h-[1000px] w-[1000px] rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#3b82f633,transparent)]"></div>
+        </div>
         <div className="absolute inset-0 z-0">
             <Ballpit />
         </div>
