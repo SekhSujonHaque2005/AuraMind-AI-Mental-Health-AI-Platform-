e1a3dc66627b9d382f14f624e08f83f8cac6fd27
Sekh Sujon Haque <sksujonhaque@gmail.com>
2025-09-05 20:25:23 +0000

    There is An Error in Calm Room and Resourses page like it is Scrolled le

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -131,7 +131,7 @@ export default function ResourcesPage() {
 
   return (
     <>
-    <div className="relative flex flex-col items-center min-h-screen p-4 pb-24 overflow-y-auto">
+    <div className="relative flex flex-col items-center min-h-screen p-4 pb-24 overflow-y-auto overflow-x-hidden">
       <div className="absolute inset-0 -z-10 h-full w-full">
             <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]"></div>
             <div className="absolute left-0 right-0 top-[-10%] h-[1000px] w-[1000px] rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#3b82f633,transparent)]"></div>
