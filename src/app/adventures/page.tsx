5843b54cc29c4564a6ff71d35fc06488f7b944bf
Sekh Sujon Haque <sksujonhaque@gmail.com>
2025-09-06 16:57:38 +0000

    Animation Should be above this page now below  (_for element <Primitive.

--- src/app/adventures/page.tsx
+++ src/app/adventures/page.tsx
@@ -199,7 +199,11 @@ export default function AdventuresPage() {
 
     return (
         <>
-            {celebrating && <Confetti recycle={false} numberOfPieces={300} />}
+            {celebrating && (
+                <div className="fixed inset-0 z-[9999]">
+                    <Confetti recycle={false} numberOfPieces={300} />
+                </div>
+            )}
             <div className="relative min-h-screen p-4 md:p-8 overflow-x-hidden">
                 <div className="absolute inset-0 -z-10 h-full w-full">
                     <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px]"></div>
