32d4463272174ff37f1ea1442514e274c19dc167
Sekh Sujon Haque <sksujonhaque@gmail.com>
2025-09-21 12:00:32 +0000

    here is my all api key ->
    Google Gemini Api Key -> AIzaSyAtRhuAnS6UOPFx

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_WEB3FORMS_ACCESS_KEY: process.env.WEB3FORMS_ACCESS_KEY,
+    NEXT_PUBLIC_WEB3FORMS_ACCESS_KEY: process.env.NEXT_PUBLIC_WEB3FORMS_ACCESS_KEY,
   },
   typescript: {
     ignoreBuildErrors: true,
